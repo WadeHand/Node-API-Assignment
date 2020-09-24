e79182c3853e65e4331323f1b864068a11f7e1e4
WadeHand <whand@gus.pittstate.edu>
2020-09-23 23:28:16 -0500

    /weather
    having trouble impementing the /weather thing, was trying ot copy /users but clearly that is not a good idea or soemthing. Will look at it later. its late and my head hurts.

---
+++ Instructions.js
@@ -0,0 +1,21 @@
+
+
+//Wade Hand         9/23/20
+
+
+//Create a new nodejs app using express-generator and ejs. Be sure to gitignore the node_modules folder! Create a weather API endpoint, with the route "/api/v1/weather". 
+//This endpoint should respond to GET requests. Return the following JSON from the weather endpoint:
+
+//{
+
+//    “Temp”:90.5,
+  
+//    “Unit”: “fahrenheit”,
+  
+//    “Cloudy”:false,
+  
+//    “PrecipitationChance”:0
+  
+//  }
+  
+//  Push your final solution to a github repository and submit the URL here. 
