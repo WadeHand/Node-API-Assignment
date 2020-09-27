eef06d5aae9ae971780e5837df3a014b2775f5bf
WadeHand <whand@gus.pittstate.edu>
2020-09-27 16:53:04 -0500

    Update weather.js
    It is not working 100% but the code should line up okay.

--- routes/weather.js
+++ routes/weather.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/api/v1/weather', function(req, res, next) {
 res.send({ title: 'Weather', Temperature: "90.5", Unit: "fahrenheit", Cloudy: "false", PrecipitationChance: "0"});
 });
 
